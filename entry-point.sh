1dfaff0d91744dc5323ed08bb8e25d62fe5bbbe6
Denis Baryshev <dennybaa@gmail.com>
2015-06-27 20:57:41 +0300

    move on

--- entry-point.sh
+++ entry-point.sh
@@ -2,23 +2,30 @@
 
 export WHEELDIR=/tmp/wheelhouse
 export TERM=xterm
-export ST2_COMPONENTS=(st2actions st2api)
+export ST2_COMPONENTS=(st2actions st2api st2auth st2client st2reactor)
 
 build_debian () {
-  BUILD_LIST=${@:-$ST2_COMPONENTS}
+  BUILD_LIST=(${@:-${ST2_COMPONENTS[@]}})
   cd /code
 
-  for makedir in "${BUILD_LIST[@]}"; do
+  echo "++++++++++++++++++++++++++++++++++++"
+  echo "${BUILD_LIST[@]}"
+
+  for makedir in ${BUILD_LIST[@]}; do
+    echo
+    echo "--------------- Creating debian package for ${makedir} ---------------"
     pushd $makedir && \
       dpkg-buildpackage -b -uc -us && \
       popd
   done
+  mv /code/*.deb /code/*.changes /code/*.dsc /out
 }
 
 # --- Merge new debian makefiles with upstream
 cp -r /sources/* /code
 
-# --- Build st2common wheel
+echo
+echo "--------------- Creating st2common wheel (in ${WHEELDIR}) ---------------"
 cd /code/st2common && \
   make wheelhouse && \
   python setup.py bdist_wheel -d $WHEELDIR 
