776677c8ab00a661e8641aa96ca7c6141d48167b
Denis Baryshev <dennybaa@gmail.com>
2015-07-09 17:47:28 +0300

    teston

--- .drone/build.sh
+++ .drone/build.sh
@@ -6,8 +6,11 @@
 set -e
 
 alias ssh='ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null'
-cat /etc/hosts
-pwd
-echo ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null busybee@$BUILDHOST cat /etc/hosts
-ssh busybee@$BUILDHOST cat /etc/hosts || true
-ssh busybee@$BUILDHOST cat /test.file || true
+
+alias | grep ssh
+
+echo ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null busybee@$BUILDHOST cat /etc/hosts || true
+echo ssh busybee@$BUILDHOST cat /etc/hosts || true
+echo ssh busybee@$BUILDHOST cat /test.file || true
+
+sleep infinity
