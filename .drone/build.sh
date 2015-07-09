0aa0b07877dfa760c721735510fa82c4e0b9b592
Denis Baryshev <dennybaa@gmail.com>
2015-07-09 21:00:25 +0300

    a

--- .drone/build.sh
+++ .drone/build.sh
@@ -5,5 +5,6 @@
 
 set -e
 
+alias ssh="ssh -i /root/.ssh/busybee -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
 
-ssh -i /root/.ssh/busybee -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null busybee@$BUILDHOST cat /etc/hosts || true
+ssh busybee@$BUILDHOST cat /etc/hosts || true
