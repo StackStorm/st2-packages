5eb5a87d35d11096b8ac32b6b624a04ecc2435cd
Denis Baryshev <dennybaa@gmail.com>
2015-07-09 20:20:18 +0300

    yoloyolo

--- .drone/build.sh
+++ .drone/build.sh
@@ -6,11 +6,6 @@
 set -e
 
 alias ssh='ssh -i /root/.ssh/busybee -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null'
-
 alias | grep ssh
 
-cat /root/.ssh/id_rsa
-ssh busybee@$BUILDHOST cat /test.file || true
-
-sleep infinity
-# hmm odd
+ssh busybee@$BUILDHOST cat /etc/hosts
