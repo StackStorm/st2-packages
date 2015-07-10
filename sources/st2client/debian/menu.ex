55ebd8525dbaaaef926e1b7392171237ee65c2cf
Denis Baryshev <dennybaa@gmail.com>
2015-07-10 19:01:10 +0300

    st2client debianized

--- sources/st2client/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(st2client):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="st2client" command="/usr/bin/st2client"
--- sources/st2client/debian/st2client.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the st2client package
-#
-0 4	* * *	root	[ -x /usr/bin/st2client_maintenance ] && /usr/bin/st2client_maintenance
