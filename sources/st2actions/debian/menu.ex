c71671bbeb0eb2a748ea639353c2e1c76e7e7ff3
Denis Baryshev <dennybaa@gmail.com>
2015-07-10 21:37:38 +0300

    removed st2action debian/*.ex

--- sources/st2actions/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(st2actions):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="st2actions" command="/usr/bin/st2actions"
--- sources/st2actions/debian/st2actions.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the st2actions package
-#
-0 4	* * *	root	[ -x /usr/bin/st2actions_maintenance ] && /usr/bin/st2actions_maintenance
