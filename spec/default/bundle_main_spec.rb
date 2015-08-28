44cc1c190045de3751d4412e612eec501766ce4d
Denis Baryshev <dennybaa@gmail.com>
2015-08-28 19:33:39 +0300

    register-content verbose when fails

--- spec/default/bundle_main_spec.rb
+++ spec/default/bundle_main_spec.rb
@@ -3,16 +3,7 @@ require 'spec_helper'
 # Bring up all stackstorm services
 #
 shared_examples 'prepare st2 services' do
-  before(:all) do
-    puts '===> Starting st2 services...'
-    remote_start_services(spec[:service_list])
 
-    puts "===> Wait for st2 services to start #{spec[:wait_for_start]} sec..."
-    sleep spec[:wait_for_start]
-
-    puts "===> Register st2 content"
-    remote_register_content
-  end
 end
 
 # Share example showing remote logs of failed st2 services
@@ -48,27 +39,43 @@ end
 
 # Prepare
 describe 'st2 prepare' do
-  include_examples 'prepare st2 services'
+  before(:all) do
+    puts '===> Starting st2 services...'
+    remote_start_services(spec[:service_list])
+
+    puts "===> Wait for st2 services to start #{spec[:wait_for_start]} sec..."
+    sleep spec[:wait_for_start]
+  end
+  register_content = ::File.join(spec[:bin_prefix],
+                                 'st2-register-content --register-all ' \
+                                 "--config-dir #{spec[:conf_dir]}")
+
+  # buggy buggy netcat and serverspec!
+  describe host(ENV['RABBITMQHOST']) do
+    it { is_expected.to be_reachable }
+    # the next is buggy :(, thus disabled
+    # it { is_expected.to be_reachable.with(port: 5672) }
+  end
+
+  describe host(ENV['MONGODBHOST']) do
+    it { is_expected.to be_reachable }
+    # it { is_expected.to be_reachable.with(port: 27_017) }
+  end
+
+  describe command(register_content) do
+    after(:all) do
+      if described_class.exit_status > 0
+        puts "\nRegister content has failed (:", '>>>>>',
+             described_class.stderr
+      end
+    end
+    its(:exit_status) { is_expected.to eq 0 }
+  end
 end
 
 # Services check up
 describe 'st2 services check' do
-  include_examples 'prepare st2 services'
   include_examples 'show service log on failure'
-
-  context 'external' do
-    # buggy buggy netcat and serverspec!
-    describe host(ENV['RABBITMQHOST']) do
-      it { is_expected.to be_reachable }
-      # the next is buggy :(, thus disabled
-      # it { is_expected.to be_reachable.with(port: 5672) }
-    end
-
-    describe host(ENV['MONGODBHOST']) do
-      it { is_expected.to be_reachable }
-      # it { is_expected.to be_reachable.with(port: 27_017) }
-    end
-  end
 
   spec[:service_list].each do |name|
     describe service(name), prompt_on_failure: true do
@@ -78,7 +85,7 @@ describe 'st2 services check' do
   end
 end
 
-describe 'st2 simple checks' do
+describe 'st2 simple check' do
   context 'actions' do
     describe command("st2 run core.local -- hostname") do
       its(:exit_status) { is_expected.to eq 0 }
