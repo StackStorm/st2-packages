6a4fda37d7b3b52f0bb05e8d77d3f34312c6f562
Denis Baryshev <dennybaa@gmail.com>
2015-09-14 19:52:52 +0300

    removed obsolete scripts/st2pkg_version.sh

--- scripts/st2pkg_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-debug "$BASH_SOURCE has been sourced!"
-
-# We need to extract version or use environment var if it's given
-make populate_version
-version=$(python -c "from $1 import __version__; print __version__,")
-export ST2PKG_VERSION=${ST2PKG_VERSION:-${version}}
