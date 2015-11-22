b62081e60461ff7efa71c412ba28b6bd5eeee858
Denis Baryshev <dennybaa@gmail.com>
2015-11-22 23:36:26 +0300

    renamed .circle/wheelhouse.sh -> .circle/cache-permissions.sh

--- .circle/cache-permissions.sh
+++ .circle/cache-permissions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Wheelhouse script is run at dependencies:pre stage, thus
+# Script is run at dependencies:pre stage, thus
 # it's invoked after cache restore.
 set -e
 
-# Make ubuntu's .cache/pip owned by root
+# Make ubuntu's .cache/pip and wheelhouse owned by root
 mkdir -p ~/.cache/pip && sudo chown -R root.root ~/.cache/pip
 mkdir -p ~/wheelhouse && sudo chown -R root.root ~/wheelhouse
