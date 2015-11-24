cf78f3815f5beaf1e1009e89d0c77de392bb69cc
Denis Baryshev <dennybaa@gmail.com>
2015-11-25 01:25:12 +0300

    aaa

--- .circle/compose.sh
+++ .circle/compose.sh
@@ -15,6 +15,9 @@ fi
 # Run compose only if args provided, otherwise just the code above
 # will be executed.
 if [ $# -gt 0 ]; then
+  echo "========================"
   echo docker-compose -f compose.yml -f docker-compose.circle.yml $CMD ${DISTROS[$B_IDX]} "$@"
   docker-compose -f compose.yml -f docker-compose.circle.yml $CMD ${DISTROS[$B_IDX]} "$@"
 fi
+
+echo "--------------------"
