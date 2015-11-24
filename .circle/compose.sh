c0bff590b7d6b1f2e2136cefa1d3b66ea25da2d6
Denis Baryshev <dennybaa@gmail.com>
2015-11-25 00:29:02 +0300

    fixie for build matrix

--- .circle/compose.sh
+++ .circle/compose.sh
@@ -4,6 +4,7 @@ set -e
 
 DISTROS=(wheezy jessie trusty centos7)
 B_IDX=$CIRCLE_NODE_INDEX
+CMD=$1 && shift
 
 # First invocation, so we need to install pip modules.
 if (! pip show docker-compose &>/dev/null); then
@@ -14,5 +15,6 @@ fi
 # Run compose only if args provided, otherwise just the code above
 # will be executed.
 if [ $# -gt 0 ]; then
-  docker-compose -f compose.yml -f docker-compose.circle.yml ${DISTROS[$B_IDX]} "$@"
+  echo docker-compose -f compose.yml -f docker-compose.circle.yml $CMD ${DISTROS[$B_IDX]} "$@"
+  docker-compose -f compose.yml -f docker-compose.circle.yml $CMD ${DISTROS[$B_IDX]} "$@"
 fi
