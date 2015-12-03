1e7111be0e9a18af8771de5b714e307ddf2f2ae6
Denis Baryshev <dennybaa@gmail.com>
2015-12-03 04:28:27 +0300

    Pass Services host from CI to docker-compose via CLI
    Avoid warnings for non-ci run
    
    Conflicts:
    	.circle/docker-compose.sh

--- .circle/docker-compose.sh
+++ .circle/docker-compose.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 # Used for `RABBITMQHOST` `POSTGRESHOST` `MONGODBHOST`, see docker-compose.override.yml
-export HOST_IP=$(ifconfig docker0 | grep 'inet addr' | awk -F: '{print $2}' | awk '{print $1}')
+HOST_IP=$(ifconfig docker0 | grep 'inet addr' | awk -F: '{print $2}' | awk '{print $1}')
 
 # Pass these ENV Variables for `docker-compose` to consume:
 # ST2_GITURL - st2 GitHub repository (ex: https://github.com/StackStorm/st2)
@@ -11,6 +11,10 @@ export HOST_IP=$(ifconfig docker0 | grep 'inet addr' | awk -F: '{print $2}' | aw
 # ST2PKG_VERSION - st2 version, will be reused in Docker image metadata (ex: 1.2dev)
 # ST2PKG_RELEASE - Release number aka revision number for `st2bundle` package, will be reused in Docker metadata (ex: 4)
 
+# Usage:
+# docker-compose.sh pull wheezy - Pull dependant Docker Images for wheezy
+# docker-compose.sh build wheezy - Build packages for specific distro
+# docker-compose.sh test wheezy - Perform Serverspec/Rspec tests for wheezy
 case "$1" in
   # Perform fake command invocation, technically provides images "pull" phase.
   pull)
@@ -19,7 +23,11 @@ case "$1" in
         -e ST2_GITURL=${ST2_GITURL} \
         -e ST2_GITREV=${ST2_GITREV} \
         -e ST2PKG_VERSION=${ST2PKG_VERSION} \
-        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} $2 /bin/true
+        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
+        -e RABBITMQHOST=${HOST_IP} \
+        -e POSTGRESHOST=${HOST_IP} \
+        -e MONGODBHOST=${HOST_IP} \
+        $2 /bin/true
   ;;
   build)
     echo Starting Packages Build for $2 ...
@@ -27,7 +35,11 @@ case "$1" in
         -e ST2_GITURL=${ST2_GITURL} \
         -e ST2_GITREV=${ST2_GITREV} \
         -e ST2PKG_VERSION=${ST2PKG_VERSION} \
-        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} $2 build
+        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
+        -e RABBITMQHOST=${HOST_IP} \
+        -e POSTGRESHOST=${HOST_IP} \
+        -e MONGODBHOST=${HOST_IP} \
+        $2 build
   ;;
   test)
     re="\\b$2\\b"
@@ -37,7 +49,11 @@ case "$1" in
           -e ST2_GITURL=${ST2_GITURL} \
           -e ST2_GITREV=${ST2_GITREV} \
           -e ST2PKG_VERSION=${ST2PKG_VERSION} \
-          -e ST2PKG_RELEASE=${ST2PKG_RELEASE} $2 test
+          -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
+          -e RABBITMQHOST=${HOST_IP} \
+          -e POSTGRESHOST=${HOST_IP} \
+          -e MONGODBHOST=${HOST_IP} \
+          $2 test
     else
       echo Omitting Tests for $2 ...
     fi
