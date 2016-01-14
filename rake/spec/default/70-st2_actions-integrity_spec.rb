3c94f533285aae6ab4499bb7ec32265278b803a4
Denis Baryshev <dennybaa@gmail.com>
2016-01-14 18:57:01 +0700

    added packs.install test to rspec

--- rake/spec/default/70-st2_actions-integrity_spec.rb
+++ rake/spec/default/70-st2_actions-integrity_spec.rb
@@ -2,4 +2,8 @@ describe 'st2 actions integrity checks' do
   describe command("st2 run core.local -- hostname") do
     its(:exit_status) { is_expected.to eq 0 }
   end
+
+  describe command("st2 run packs.install packs=github") do
+    its(:exit_status) { is_expected.to eq 0 }
+  end
 end
