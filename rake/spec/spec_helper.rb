21c177c461e7cfa4099ea85ffd697c73674accf4
armab <dev@arma.biz>
2016-01-27 17:05:37 +0200

    Again, try lifting waitforstart to 15 sec

--- rake/spec/spec_helper.rb
+++ rake/spec/spec_helper.rb
@@ -36,7 +36,7 @@ class ST2Spec
     rabbitmqhost: pipeopts.rabbitmqhost,
     postgreshost: pipeopts.postgreshost,
     mongodbhost:  pipeopts.mongodbhost,
-    wait_for_start: ENV['ST2_WAITFORSTART'].to_s != '' ? ENV['ST2_WAITFORSTART'].to_i : 7,
+    wait_for_start: ENV['ST2_WAITFORSTART'].to_s != '' ? ENV['ST2_WAITFORSTART'].to_i : 15,
     loglines_to_show: 20,
     logdest_pattern: {
       st2actionrunner: 'st2actionrunner.{pid}'
