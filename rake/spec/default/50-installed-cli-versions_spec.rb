575dbdd4da00d452bf70ca701388efd1ca2d2686
armab <dev@arma.biz>
2016-02-05 17:17:51 +0200

    Show st2/mistral version numbers

--- rake/spec/default/50-installed-cli-versions_spec.rb
+++ rake/spec/default/50-installed-cli-versions_spec.rb
@@ -1,11 +1,16 @@
+require 'spec_helper'
+
 describe 'st2 cli version checks' do
   describe command("st2 --version") do
+    puts described_class.stdout
+    puts described_class.stderr
     its(:exit_status) { is_expected.to eq 0 }
   end
 
   if spec[:mistral_enabled]
     describe command("mistral --version") do
       its(:exit_status) { is_expected.to eq 0 }
+      puts described_class.stdout
     end
   end
 end
