5e6ded8c4fae1b3e356cf6058aac4903f187ec99
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2016-02-26 17:42:15 -0800

    Make u14 script consistent with el*

--- scripts/st2bootstrap-deb.sh
+++ scripts/st2bootstrap-deb.sh
@@ -1,91 +1,123 @@
 #!/bin/bash
 
 fail() {
-	echo "############### ERROR ###############"
-	echo "# Failed on $1 #"
-	echo "#####################################"
-	exit 2
+    echo "############### ERROR ###############"
+    echo "# Failed on $1 #"
+    echo "#####################################"
+    exit 2
 }
 
-install_dependencies() {
-	sudo apt-get update
-	sudo apt-get install -y curl mongodb-server rabbitmq-server postgresql
+install_st2_dependencies() {
+    sudo apt-get update
+    sudo apt-get install -y curl mongodb-server rabbitmq-server
 }
 
-setup_repositories() {
+install_st2() {
     # Following script adds a repo file, registers gpg key and runs apt-get update
     curl -s https://packagecloud.io/install/repositories/StackStorm/staging-stable/script.deb.sh | sudo bash
+    sudo apt-get install -y st2
+    sudo st2ctl reload
+    sudo st2ctl start
 }
 
-install_stackstorm_components() {
-	sudo apt-get install -y st2 st2mistral
+configure_st2_user () {
+
+    # Create an SSH system user
+    sudo useradd stanley
+    sudo mkdir -p /home/stanley/.ssh
+
+    # Generate ssh keys on StackStorm box and copy over public key into remote box.
+    sudo ssh-keygen -f /home/stanley/.ssh/stanley_rsa -P ""
+    #sudo cp ${KEY_LOCATION}/stanley_rsa.pub /home/stanley/.ssh/stanley_rsa.pub
+
+    # Authorize key-base acces
+    sudo cat /home/stanley/.ssh/stanley_rsa.pub | sudo tee -a /home/stanley/.ssh/authorized_keys
+    sudo chmod 0600 /home/stanley/.ssh/authorized_keys
+    sudo chmod 0700 /home/stanley/.ssh
+    sudo chown -R stanley:stanley /home/stanley
+
+    # Enable passwordless sudo
+    sudo echo "stanley    ALL=(ALL)       NOPASSWD: SETENV: ALL" | sudo tee -a /etc/sudoers.d/st2
+    sudo chmod 0440 /etc/sudoers.d/st2
+
+    ##### NOTE STILL NEED ADJUST CONFIGURATION FOR ST2 USER SECTION #####
 }
 
-setup_mistral_database() {
-	cat << EHD | sudo -u postgres psql
+configure_st2_authentication() {
+    # Install htpasswd and tool for editing ini files
+    sudo apt-get install -y apache2-utils crudini
+
+    # Create a user record in a password file.
+    sudo echo "Ch@ngeMe" | sudo htpasswd -i /etc/st2/htpasswd test
+
+    # Configure [auth] section in st2.conf
+    sudo crudini --set /etc/st2/st2.conf auth enable 'True'
+    sudo crudini --set /etc/st2/st2.conf auth backend 'flat_file'
+    sudo crudini --set /etc/st2/st2.conf auth backend_kwargs '{"file_path": "/etc/st2/htpasswd"}'
+
+    sudo st2ctl restart-component st2api
+}
+
+install_st2mistral_depdendencies() {
+  sudo apt-get install -y postgresql
+
+  cat << EHD | sudo -u postgres psql
 CREATE ROLE mistral WITH CREATEDB LOGIN ENCRYPTED PASSWORD 'StackStorm';
 CREATE DATABASE mistral OWNER mistral;
 EHD
-
-	# Setup Mistral DB tables, etc.
-	/opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf upgrade head
-	# Register mistral actions
-	/opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf populate
 }
 
-configure_ssh_and_sudo () {
+install_st2mistral() {
+  # install mistral
+  sudo apt-get install -y st2mistral
 
-	# Create an SSH system user
-	sudo useradd stanley
-	sudo mkdir -p /home/stanley/.ssh
-
-	# Generate ssh keys on StackStorm box and copy over public key into remote box.
-	sudo ssh-keygen -f /home/stanley/.ssh/stanley_rsa -P ""
-	#sudo cp ${KEY_LOCATION}/stanley_rsa.pub /home/stanley/.ssh/stanley_rsa.pub
-
-	# Authorize key-base acces
-	sudo cat /home/stanley/.ssh/stanley_rsa.pub | sudo tee -a /home/stanley/.ssh/authorized_keys
-	sudo chmod 0600 /home/stanley/.ssh/authorized_keys
-	sudo chmod 0700 /home/stanley/.ssh
-	sudo chown -R stanley:stanley /home/stanley
-
-	# Enable passwordless sudo
-	sudo echo "stanley    ALL=(ALL)       NOPASSWD: SETENV: ALL" | sudo tee -a /etc/sudoers.d/st2
-    sudo chmod 0440 /etc/sudoers.d/st2
-
-	##### NOTE STILL NEED ADJUST CONFIGURATION FOR ST2 USER SECTION #####
+  # Setup Mistral DB tables, etc.
+  /opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf upgrade head
+  # Register mistral actions
+  /opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf populate
 }
 
-use_st2ctl() {
-	sudo st2ctl $1
+install_st2web() {
+    # Install st2web and nginx
+    sudo apt-get install -y st2web nginx
+
+    # Generate self-signed certificate or place your existing certificate under /etc/ssl/st2
+    sudo mkdir -p /etc/ssl/st2
+    sudo openssl req -x509 -newkey rsa:2048 -keyout /etc/ssl/st2/st2.key -out /etc/ssl/st2/st2.crt \
+    -days XXX -nodes -subj "/C=US/ST=California/L=Palo Alto/O=StackStorm/OU=Information \
+    Technology/CN=$(hostname)"
+
+    # Remove default site, if present
+    sudo rm /etc/nginx/sites-enabled/default
+    # Copy and enable StackStorm's supplied config file
+    sudo cp /usr/share/doc/st2/conf/nginx/st2.conf /etc/nginx/sites-available/
+    sudo ln -s /etc/nginx/sites-available/st2.conf /etc/nginx/sites-enabled/st2.conf
+
+    sudo service nginx restart
 }
 
-configure_authentication() {
-	sed -i '/^\[auth\]$/,/^\[/ s/^enable = False/enable = True/' /etc/st2/st2.conf
-	# Install htpasswd utility if you don't have it
-	sudo apt-get install -y apache2-utils
-	# Create a user record in a password file.
-	sudo echo "Ch@ngeMe" | sudo htpasswd -i /etc/st2/htpasswd test
+verify_st2() {
+  st2 --version
+  st2 -h
 
-}
+  st2 auth test -p Ch@ngeMe
+  # A shortcut to authenticate and export the token
+  export ST2_AUTH_TOKEN=$(st2 auth test -p Ch@ngeMe -t)
 
-install_webui_and_setup_ssl_termination() {
-	# Install st2web and nginx
-	sudo apt-get install -y st2web nginx
+  # List the actions from a 'core' pack
+  st2 action list --pack=core
 
-	# Generate self-signed certificate or place your existing certificate under /etc/ssl/st2
-	sudo mkdir -p /etc/ssl/st2
-	sudo openssl req -x509 -newkey rsa:2048 -keyout /etc/ssl/st2/st2.key -out /etc/ssl/st2/st2.crt \
-	-days XXX -nodes -subj "/C=US/ST=California/L=Palo Alto/O=StackStorm/OU=Information \
-	Technology/CN=$(hostname)"
+  # Run a local shell command
+  st2 run core.local -- date -R
 
-	# Remove default site, if present
-	sudo rm /etc/nginx/sites-enabled/default
-	# Copy and enable StackStorm's supplied config file
-	sudo cp /usr/share/doc/st2/conf/nginx/st2.conf /etc/nginx/sites-available/
-	sudo ln -s /etc/nginx/sites-available/st2.conf /etc/nginx/sites-enabled/st2.conf
+  # See the execution results
+  st2 execution list
 
-	sudo service nginx restart
+  # Fire a remote comand via SSH (Requires passwordless SSH)
+  st2 run core.remote hosts='127.0.0.1' -- uname -a
+
+  # Install a pack
+  st2 run packs.install packs=st2
 }
 
 ok_message() {
@@ -115,32 +147,17 @@ ok_message() {
     echo "http://stackstorm.com/community-signup"
 }
 
-verify() {
-
-	st2 auth test -p Ch@ngeMe || fail "st2 auth test"
-	# A shortcut to authenticate and export the token
-	export ST2_AUTH_TOKEN=$(st2 auth test -p Ch@ngeMe -t)
-
-	st2 --version || fail "st2 --version"
-	st2 -h || fail "st2 -h"
-	st2 action list --pack=core || fail "st2 action list"
-	st2 run core.local -- date -R || fail "st2 run core.local -- date -R"
-	st2 execution list || fail "st2 execution list"
-	st2 run core.remote hosts='127.0.0.1' -- uname -a || fail "st2 run core.remote hosts='127.0.0.1' -- uname -a"
-	st2 run packs.install packs=st2 || fail "st2 run packs.install packs=st2"
-    ok_message
-
-}
-
 ## Let's do this!
 
-install_dependencies || fail "install_dependencies"
-setup_repositories || fail "setup_repositories"
-install_stackstorm_components || fail "install_stackstorm_components"
-setup_mistral_database || fail "setup_mistral_database"
-configure_ssh_and_sudo || fail "configure_ss_and_sudo"
-configure_authentication || fail "configure_authentication"
-install_webui_and_setup_ssl_termination || fail "install_webui_and_setup_ssl_termination"
-use_st2ctl start || fail "use_st2ctl start"
-use_st2ctl reload || fail "use_st2ctl reload"
-verify
+install_st2_dependencies || fail "install_st2_dependencies"
+install_st2 || fail "install_st2"
+configure_st2_user || fail "configure_st2_user"
+configure_st2_authentication || fail "configure_st2_authentication"
+verify_st2 || fail "verify_st2"
+
+install_st2mistral_depdendencies || fail "install_st2mistral_depdendencies"
+install_st2mistral || fail "install_st2mistral"
+
+install_st2web || fail "install_st2web"
+
+ok_message
