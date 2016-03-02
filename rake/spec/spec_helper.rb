d19c617ce1ff56e2b4c9063a951ea2216f8c72fb
Tomaz Muraus <tomaz@tomaz.me>
2016-03-02 19:05:34 +0100

    Wait longer for services to start.

--- rake/spec/spec_helper.rb
+++ rake/spec/spec_helper.rb
@@ -37,7 +37,7 @@ class ST2Spec
     postgreshost: pipeopts.postgreshost,
     mongodbhost:  pipeopts.mongodbhost,
     # NB!!! We shouldn't change circle.yml for every branch of `st2`! Hardcode value here so it will work for all tests consistently!
-    wait_for_start: ENV['ST2_WAITFORSTART'].to_s != '' ? ENV['ST2_WAITFORSTART'].to_i : 7,
+    wait_for_start: ENV['ST2_WAITFORSTART'].to_s != '' ? ENV['ST2_WAITFORSTART'].to_i : 12,
     loglines_to_show: 20,
     logdest_pattern: {
       st2actionrunner: 'st2actionrunner.{pid}'
