b910960bf2dd5ce78cd41acab73e1cbe9b6cd75b
Patrick Hoolboom <patrick@stackstorm.com>
2016-03-03 15:20:00 -0800

    Update st2_bootstrap_selector.sh

--- scripts/st2_bootstrap_selector.sh
+++ scripts/st2_bootstrap_selector.sh
@@ -2,10 +2,79 @@
 
 BASE_PATH="https://raw.githubusercontent.com/StackStorm/st2-packages/master/scripts/st2bootstrap"
 BOOTSTRAP_FILE='st2bootstrap.sh'
-ST2VER=${ST2VER:-""}
 
 DEBTEST=`lsb_release -a 2> /dev/null | grep Distributor | awk '{print $3}'`
 RHTEST=`cat /etc/redhat-release 2> /dev/null | sed -e "s~\(.*\)release.*~\1~g"`
+
+setup_args() {
+  for i in "$@"
+    do
+      case $i in
+          -V=*|--version=*)
+          VERSION="${i#*=}"
+          shift
+          ;;
+          -s=*|--stable)
+        RELEASE=stable
+          shift
+          ;;
+          -u=*|--unstable)
+        RELEASE=unstable
+          shift
+          ;;
+          --staging)
+        REPO_TYPE='staging'
+        shift
+        ;;
+          *)
+                  # unknown option
+          ;;
+      esac
+    done
+
+  if [[ "$RELEASE" == "unstable" ]]; then
+    echo "This script does not support installing from unstable sources!"
+    # XXX: Fix this when st2mistral unstable sources become available!
+    exit 1
+  fi
+
+  if [[ "$VERSION" != '' ]]; then
+    if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+dev$ ]]; then
+      echo "$VERSION does not match supported formats x.y.z or x.ydev"
+      exit 1
+    fi
+
+    if [[ "$VERSION" =~ ^[0-9]+\.[0-9]+dev$ ]]; then
+     echo "You're requesting a dev version! Switching to unstable!"
+     RELEASE='unstable'
+    fi
+  fi
+
+  echo "########################################################"
+  echo "          Installing st2 $RELEASE $VERSION              "
+  echo "########################################################"
+
+  if [[ -z "$BETA"  && "$REPO_TYPE"="staging" ]]; then
+    printf "\n\n"
+    echo "################################################################"
+    echo "### Installing from staging repos!!! USE AT YOUR OWN RISK!!! ###"
+    echo "################################################################"
+  fi
+}
+
+setup_args
+
+if [[ "$VERSION" != '' ]]; then
+  VERSION="--version ${VERSION}"
+fi
+
+if [[ "$RELEASE" != '' ]]; then
+  RELEASE="--version ${RELEASE}"
+fi
+
+if [[ "$REPO_TYPE" == 'staging' ]]; then
+  REPO_TYPE="--staging"
+fi
 
 if [[ -n "$DEBTEST" ]]; then
   TYPE="debs"
@@ -26,13 +95,13 @@ CURLTEST=`curl --output /dev/null --silent --head --fail ${ST2BOOTSTRAP}`
 if [ $? -ne 0 ]; then
     echo -e "Could not find file ${ST2BOOTSTRAP}"
     exit 2
-#else
-#    echo "Downloading deployment script from: ${ST2BOOTSTRAP}..."
-#    curl -Ss -k -o ${BOOTSTRAP_FILE} ${ST2BOOTSTRAP}
-#    chmod +x ${BOOTSTRAP_FILE}
-#
-#    echo "Running deployment script for St2 ${ST2VER}..."
-#    bash ${BOOTSTRAP_FILE} ${ST2VER}
+else
+    echo "Downloading deployment script from: ${ST2BOOTSTRAP}..."
+    curl -Ss -k -o ${BOOTSTRAP_FILE} ${ST2BOOTSTRAP}
+    chmod +x ${BOOTSTRAP_FILE}
+
+    echo "Running deployment script for St2 ${VERSION}..."
+    bash ${BOOTSTRAP_FILE} ${VERSION} ${RELEASE} ${REPO_TYPE}
 fi
 
 echo "${ST2BOOTSTRAP}"
