8466360ddbc3e0ecbe1cfb81887d54c415fbe2d2
armab <dev@arma.biz>
2016-03-08 19:08:18 +0200

    Follow st2 branching
    Fetch specific versioned `st2` branch depending on current `st2-packages` branch

--- .circle/buildenv.sh
+++ .circle/buildenv.sh
@@ -4,7 +4,29 @@ set -e
 distros=($DISTROS)
 DISTRO=${distros[$CIRCLE_NODE_INDEX]}
 
-fetch_version() {
+
+# Discover st2 giturl, eg. the codebase we work with
+# In case of forked PR - it will use user's `st2` giturl
+get_st2_giturl() {
+  # Handle pull requests properly
+  if [ -z "$CIRCLE_PR_REPONAME" ]; then
+    echo "https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
+  else
+    echo "https://github.com/${CIRCLE_PR_USERNAME}/${CIRCLE_PR_REPONAME}"
+  fi
+}
+
+# Discover the st2 branch we work with
+get_st2_gitrev() {
+  if echo "${CIRCLE_BRANCH}" | grep -q '^v[0-9]\+\.[0-9]\+$'; then
+    echo "${CIRCLE_BRANCH}"
+  else
+    echo "master"
+  fi
+}
+
+# Discover st2 version based on st2common `__init__` file
+get_st2_version() {
   if [ -f ../st2common/st2common/__init__.py ]; then
     # Get st2 version based on hardcoded string in st2common
     # build takes place in `st2` repo
@@ -13,16 +35,6 @@ fetch_version() {
     # build takes place in `st2-packages` repo
     curl -sSL -o /tmp/st2_version.py ${ST2_GITURL}/raw/${ST2_GITREV}/st2common/st2common/__init__.py
     python -c 'execfile("/tmp/st2_version.py"); print __version__'
-  fi
-}
-
-# Needs explantion???
-st2_giturl() {
-  # Handle pull requests properly
-  if [ -z "$CIRCLE_PR_REPONAME" ]; then
-    echo "https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
-  else
-    echo "https://github.com/${CIRCLE_PR_USERNAME}/${CIRCLE_PR_REPONAME}"
   fi
 }
 
@@ -42,9 +54,9 @@ write_env() {
 # ST2PKG_RELEASE - Release number aka revision number for `st2` package, will be reused in Docker metadata (ex: 4)
 # ST2_WAITFORSTART - Delay between st2 start and service checks
 
-ST2_GITURL=${ST2_GITURL:-$(st2_giturl)}
-ST2_GITREV=${ST2_GITREV:-$CIRCLE_BRANCH}
-ST2PKG_VERSION=$(fetch_version)
+ST2_GITURL=${ST2_GITURL:-$(get_st2_giturl)}
+ST2_GITREV=${ST2_GITREV:-$(get_st2_gitrev)}
+ST2PKG_VERSION=$(get_st2_version)
 # for Bintray
 #ST2PKG_RELEASE=$(.circle/bintray.sh next-revision ${DISTRO}_staging ${ST2PKG_VERSION} st2)
 # for PackageCloud
