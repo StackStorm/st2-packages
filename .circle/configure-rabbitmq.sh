c7ba30b3740a314e97693f2e60f565b8719a0b2d
manasdk <manas@stackstorm.com>
2016-03-25 14:42:56 -0700

    remove unnecessary escapes

--- .circle/configure-rabbitmq.sh
+++ .circle/configure-rabbitmq.sh
@@ -2,7 +2,7 @@
 
 # Enable remote guest access
 CONFIG=$(cat <<EHD
-[{rabbit, [{disk_free_limit, 10}, {loopback_users, []}, {tcp_listeners, [{\"0.0.0.0\", 5672}]}]}].
+[{rabbit, [{disk_free_limit, 10}, {loopback_users, []}, {tcp_listeners, [{"0.0.0.0", 5672}]}]}].
 EHD
 )
 
