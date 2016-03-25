ee83d3b15984202d979ccd302b5f2866ec975562
manasdk <manas@stackstorm.com>
2016-03-25 14:18:08 -0700

    Remove unnecessary configure script

--- .circle/configure-test-rabbitmq.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo sh -c 'echo "[{rabbit, [{disk_free_limit, 10}, {loopback_users, []}, {tcp_listeners, [{\"0.0.0.0\", 5672}]}]}]." > /etc/rabbitmq/rabbitmq.config'
-sudo service rabbitmq-server restart
