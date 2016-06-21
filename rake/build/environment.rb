14fb5e037a1cc09ca0f511b43ccb42ea5defc5c5
Winson Chan <m4d.coder@gmail.com>
2016-06-21 22:35:08 +0000

    Update version info for release - 1.5.0

--- rake/build/environment.rb
+++ rake/build/environment.rb
@@ -80,8 +80,8 @@ end
 pipeopts 'st2mistral' do
   checkout true
   envpass :giturl,  'https://github.com/StackStorm/mistral', from: 'ST2MISTRAL_GITURL'
-  envpass :gitrev,  'st2-1.4.0',                             from: 'ST2MISTRAL_GITREV'
+  envpass :gitrev,  'st2-1.5.0',                             from: 'ST2MISTRAL_GITREV'
   envpass :gitdir,  make_tmpname('mistral-')
-  envpass :mistral_version, '1.4.0'
+  envpass :mistral_version, '1.5.0'
   envpass :mistral_release, 1
 end
