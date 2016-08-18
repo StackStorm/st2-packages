109ea7e39545117bb2fee56de35e519a5ad00807
Tomaz Muraus <tomaz@tomaz.me>
2016-08-18 09:01:50 +0200

    Use more variables.

--- scripts/st2bootstrap-deb.sh
+++ scripts/st2bootstrap-deb.sh
@@ -253,14 +253,16 @@ EOT"
 }
 
 generate_symmetric_crypto_key_for_datastore() {
-  DATASTORE_ENCRYPTION_KEY_PATH="/etc/st2/keys/datastore_key.json"
+  DATASTORE_ENCRYPTION_KEYS_DIRECTORY="/etc/st2/keys"
+  DATASTORE_ENCRYPTION_KEY_PATH="${DATASTORE_ENCRYPTION_KEYS_DIRECTORY}/datastore_key.json"
 
-  sudo mkdir -p /etc/st2/keys/
+  sudo mkdir -p ${DATASTORE_ENCRYPTION_KEYS_DIRECTORY}
   sudo st2-generate-symmetric-crypto-key --key-path ${DATASTORE_ENCRYPTION_KEY_PATH}
 
   # Make sure only st2 user can read the file
-  sudo chgrp st2 /etc/st2/keys/datastore_key.json
-  sudo chmod o-r /etc/st2/keys/datastore_key.json
+  sudo usermod -a -G st2 st2
+  sudo chgrp st2 ${DATASTORE_ENCRYPTION_KEY_PATH}
+  sudo chmod o-r ${DATASTORE_ENCRYPTION_KEY_PATH}
 
   # set path to the key file in the config
   sudo crudini --set /etc/st2/st2.conf keyvalue encryption_key_path ${DATASTORE_ENCRYPTION_KEY_PATH}
