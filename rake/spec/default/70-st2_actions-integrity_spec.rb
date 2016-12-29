d3b163740efbcd499651d461108b911859cdd183
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2016-12-29 02:36:08 +0000

    added test to check handling unicode argument to an action

--- rake/spec/default/70-st2_actions-integrity_spec.rb
+++ rake/spec/default/70-st2_actions-integrity_spec.rb
@@ -6,4 +6,12 @@ describe 'st2 actions integrity checks' do
   describe command("st2 run packs.install packs=github") do
     its(:exit_status) { is_expected.to eq 0 }
   end
+
+  describe command("st2 run core.local cmd='locale'") do
+    its(:stdout) { should match /UTF-8/ }
+  end
+
+  describe command("st2 run core.local cmd='echo ｈｏｇｅ'") do
+    its(:exit_status) { is_expected.to eq 0 }
+  end
 end
