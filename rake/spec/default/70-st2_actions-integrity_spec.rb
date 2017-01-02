3a52f72fcfb5adb8e6db64505efbf28768cff4e2
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-01-02 14:19:09 +0000

    corrected characters in the test of multibyte characters for showing the purpose of this test clearly

--- rake/spec/default/70-st2_actions-integrity_spec.rb
+++ rake/spec/default/70-st2_actions-integrity_spec.rb
@@ -11,7 +11,7 @@ describe 'st2 actions integrity checks' do
     its(:stdout) { should match /UTF-8/ }
   end
 
-  describe command("st2 run core.local cmd='echo ｈｏｇｅ'") do
+  describe command("st2 run core.local cmd='echo ¯\_(ツ)_/¯'") do
     its(:exit_status) { is_expected.to eq 0 }
   end
 end
