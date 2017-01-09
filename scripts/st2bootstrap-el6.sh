d21214bbf0eac86eb86bee413f72077f7cac1c80
Tomaz Muraus <tomaz@tomaz.me>
2017-01-09 17:08:49 +0100

    Need to use echo -e on RHEL.

--- scripts/st2bootstrap-el6.sh
+++ scripts/st2bootstrap-el6.sh
@@ -334,7 +334,7 @@ quit();
 EOF
 
   # Require authentication to be able to acccess the database
-  sudo sh -c 'echo "security:\n  authorization: enabled" >> /etc/mongod.conf'
+  sudo sh -c 'echo -e "security:\n  authorization: enabled" >> /etc/mongod.conf'
 
   # MongoDB needs to be restarted after enabling auth
   sudo service mongod restart
--- scripts/st2bootstrap-el7.sh
+++ scripts/st2bootstrap-el7.sh
@@ -317,7 +317,7 @@ quit();
 EOF
 
   # Require authentication to be able to acccess the database
-  sudo sh -c 'echo "security:\n  authorization: enabled" >> /etc/mongod.conf'
+  sudo sh -c 'echo -e "security:\n  authorization: enabled" >> /etc/mongod.conf'
 
   # MongoDB needs to be restarted after enabling auth
   sudo systemctl restart mongod
