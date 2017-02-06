c9dc8750ed5411312fe031b9ca8969107a74cd0f
armab <dev@arma.biz>
2017-02-06 20:54:57 +0200

    Fix the echo command

--- rake/spec/default/70-st2_actions-integrity_spec.rb
+++ rake/spec/default/70-st2_actions-integrity_spec.rb
@@ -7,11 +7,11 @@ describe 'st2 actions integrity checks' do
     its(:exit_status) { is_expected.to eq 0 }
   end
 
-  describe command("st2 run core.local cmd='locale'") do
+  describe command("st2 run core.local cmd=locale") do
     its(:stdout) { should match /UTF-8/ }
   end
 
-  describe command("st2 run core.local cmd='echo ¯\_(ツ)_/¯'") do
+  describe command("st2 run core.local cmd=\"echo '¯\_(ツ)_/¯'\"") do
     its(:exit_status) { is_expected.to eq 0 }
   end
 end
