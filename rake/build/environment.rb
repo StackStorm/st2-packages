91587bb8aae0b72d516c769afc0d2bd98225b8b9
Eugen C <armab@users.noreply.github.com>
2017-06-15 01:31:19 +0300

    Update version info for release - 2.3.0 (fix)

--- rake/build/environment.rb
+++ rake/build/environment.rb
@@ -84,6 +84,6 @@ pipeopts 'st2mistral' do
   envpass :giturl,  'https://github.com/StackStorm/mistral', from: 'ST2MISTRAL_GITURL'
   envpass :gitrev,  'master',                                from: 'ST2MISTRAL_GITREV'
   envpass :gitdir,  make_tmpname('mistral-')
-  envpass :mistral_version, '2.4dev'
+  envpass :mistral_version, '2.3dev'
   envpass :mistral_release, 1
 end
