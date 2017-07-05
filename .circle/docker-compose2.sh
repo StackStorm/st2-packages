1a53275fc91251747104267daf5a0bdd8ec39472
armab <dev@arma.biz>
2017-07-05 19:15:39 +0300

    Docker-compose param change for Circle 2.0 [ci skip]

---
+++ .circle/docker-compose2.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Usage: docker-compose.sh OPERATION
+#   This script is a st2 packages build pipeline invocation wrapper.
+#
+# Operations:
+#   pull, build and test operations are available. Which pull containers,
+#   build and test packages respectivly.
+#
+
+set -e
+# Source the build environment defintion (details in buildenv.sh)
+. ~/.buildenv
+
+set -x
+case "$1" in
+  # Perform fake command invocation, technically provides images "pull" phase.
+  pull)
+    echo Pulling dependent Docker images for $2 ...
+    docker-compose -f docker-compose.circle2.yml run \
+        -e ST2_GITURL=${ST2_GITURL} \
+        -e ST2_GITREV=${ST2_GITREV} \
+        -e ST2PKG_VERSION=${ST2PKG_VERSION} \
+        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
+        -e ST2_CIRCLE_URL=${CIRCLE_BUILD_URL} \
+        $2 /bin/true
+  ;;
+  build)
+    echo Starting Packages Build for $2 ...
+    docker-compose -f docker-compose.circle2.yml run \
+        -e ST2_GITURL=${ST2_GITURL} \
+        -e ST2_GITREV=${ST2_GITREV} \
+        -e ST2PKG_VERSION=${ST2PKG_VERSION} \
+        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
+        -e ST2MISTRAL_GITURL=${ST2MISTRAL_GITURL} \
+        -e ST2MISTRAL_GITREV=${ST2MISTRAL_GITREV} \
+        -e MISTRAL_VERSION=${MISTRAL_VERSION} \
+        -e MISTRAL_RELEASE=${MISTRAL_RELEASE} \
+        -e ST2_PACKAGES="${ST2_PACKAGES}" \
+        -e ST2_CIRCLE_URL=${CIRCLE_BUILD_URL} \
+        $2 build
+  ;;
+  test)
+    [ "$TESTING" = 0 ] && { echo "Omitting Tests for $2 ..." ; exit 0; }
+    echo Starting Tests for $2 ...
+    docker-compose -f docker-compose.circle2.yml run \
+        -e ST2_GITURL=${ST2_GITURL} \
+        -e ST2_GITREV=${ST2_GITREV} \
+        -e ST2PKG_VERSION=${ST2PKG_VERSION} \
+        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
+        -e ST2_PACKAGES="${ST2_PACKAGES}" \
+        -e ST2_CIRCLE_URL=${CIRCLE_BUILD_URL} \
+        $2 test
+  ;;
+esac
+
