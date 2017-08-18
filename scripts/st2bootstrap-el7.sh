8ff3a5f2152db2c6113e0680626a4c579353a34a
Matt Oswalt <Mierdin@users.noreply.github.com>
2017-08-18 14:19:42 -0700

    Suppress errors in el7 mistral db populate

--- scripts/st2bootstrap-el7.sh
+++ scripts/st2bootstrap-el7.sh
@@ -514,7 +514,7 @@ install_st2mistral() {
   /opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf upgrade head
 
   # Register mistral actions.
-  /opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf populate
+  /opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf populate | grep -v openstack
 
   # start mistral
   sudo systemctl start mistral
