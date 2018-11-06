1873f41c5f7b52e35c116d2978a2d8e069722600
Tomaz Muraus <tomaz@tomaz.me>
2018-11-06 21:13:17 +0100

    Set ST2_PACKAGES environment variable inside buildenv_common.

--- .circle/buildenv_common.sh
+++ .circle/buildenv_common.sh
@@ -6,3 +6,17 @@ write_env() {
   done
   echo ". ~/.buildenv" >> ~/.circlerc
 }
+
+distros=($DISTROS)
+DISTRO=${distros[$CIRCLE_NODE_INDEX]}
+
+# NOTE: We don't build Mistral package on Ubuntu Bionic
+if [ "${DISTRO}" = "bionic"} ]; then
+    ST2_PACKAGES="st2"
+else
+    ST2_PACKAGES=${ST2_PACKAGES:-st2 mistral}
+fi
+
+write_env ST2_PACKAGES
+
+cat ~/.buildenv
