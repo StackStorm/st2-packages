55f21f95ea59834d56b0198965f446e9a5dd0dbf
Tomaz Muraus <tomaz@tomaz.me>
2018-11-06 16:04:47 +0100

    Revert "Try specifying ST2_PACKAGES env variable in docker compose files."
    This reverts commit 5897190bcad77f2eac0e69f0a5eb75e733b5c501.

--- .circle/buildenv_st2.sh
+++ .circle/buildenv_st2.sh
@@ -58,6 +58,13 @@ re="\\b$DISTRO\\b"
 
 ST2_CIRCLE_URL=${CIRCLE_BUILD_URL}
 
-write_env ST2_GITURL ST2_GITREV ST2PKG_VERSION ST2PKG_RELEASE DISTRO TESTING ST2_CIRCLE_URL
+# NOTE: We don't build Mistral package on Ubuntu Bionic
+if [ "${DISTRO}" = "bionic"} ]; then
+    ST2_PACKAGES="st2"
+else
+    ST2_PACKAGES=${ST2_PACKAGES:-st2 mistral}
+fi
+
+write_env ST2_GITURL ST2_GITREV ST2PKG_VERSION ST2PKG_RELEASE DISTRO TESTING ST2_CIRCLE_URL ST2_PACKAGES
 
 cat ~/.buildenv
