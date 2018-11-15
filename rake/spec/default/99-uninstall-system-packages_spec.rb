066517dae5c3366e9c05459707579f818c1559d7
Tomaz Muraus <tomaz@tomaz.me>
2018-11-15 22:30:48 +0100

    Revert "Add a step which checks package is installed and package has been"
    This reverts commit a098687ac28f6704cbc1297dd62d3322f15ca1c9.

--- rake/spec/default/99-uninstall-system-packages_spec.rb
+++ rake/spec/default/99-uninstall-system-packages_spec.rb
@@ -2,32 +2,12 @@
 describe 'st2 packages uninstall test' do
   spec[:package_list].each do |pkg_name|
     if os[:family] == 'redhat'
-      # Verify package is installed
-      describe command("rpm -q #{pkg_name}") do
-        its(:exit_status) { is_expected.to eq 0 }
-      end
-
       describe command("sudo yum -y remove #{pkg_name}") do
         its(:exit_status) { is_expected.to eq 0 }
       end
-
-      # Verify package has been uninstalled successfully
-      describe command("rpm -q #{pkg_name}") do
-        its(:exit_status) { is_expected.to eq 1 }
-      end
     elsif os[:family] == 'ubuntu'
-      # Verify package is installed
-      describe command("dpkg -l #{pkg_name}") do
-        its(:exit_status) { is_expected.to eq 0 }
-      end
-
       describe command("sudo apt-get remove -y --purge #{pkg_name}") do
         its(:exit_status) { is_expected.to eq 0 }
-      end
-
-      # Verify package has been uninstalled successfully
-      describe command("dpkg -l #{pkg_name}") do
-        its(:exit_status) { is_expected.to eq 1 }
       end
     end
   end
