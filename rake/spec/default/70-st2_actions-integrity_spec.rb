a9642c6bb7b9d781a3eeb9f52570423f87a933b3
Tomaz Muraus <tomaz@tomaz.me>
2018-11-19 15:14:40 +0100

    Use hubot pack which works on mistral and non-mistral installations.

--- rake/spec/default/70-st2_actions-integrity_spec.rb
+++ rake/spec/default/70-st2_actions-integrity_spec.rb
@@ -4,7 +4,7 @@ describe 'st2 actions integrity checks' do
     its(:exit_status) { is_expected.to eq 0 }
   end
 
-  describe command("st2 pack install xml") do
+  describe command("st2 pack install hubot") do
     its(:exit_status) { is_expected.to eq 0 }
   end
 
