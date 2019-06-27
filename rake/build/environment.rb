942a1daaa144a9a0042f88cb89acddde6bab6799
stanley <stanley@stackstorm.com>
2019-06-27 17:27:01 +0000

    Update version info for release - 3.1.0

--- rake/build/environment.rb
+++ rake/build/environment.rb
@@ -84,6 +84,6 @@ pipeopts 'st2mistral' do
   envpass :giturl,  'https://github.com/StackStorm/mistral', from: 'ST2MISTRAL_GITURL'
   envpass :gitrev,  'master',                                from: 'ST2MISTRAL_GITREV'
   envpass :gitdir,  make_tmpname('mistral-'),                from: 'ST2MISTRAL_GITDIR'
-  envpass :mistral_version, '3.1dev'
+  envpass :mistral_version, '3.2dev'
   envpass :mistral_release, 1
 end
