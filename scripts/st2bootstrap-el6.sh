070f9f6b43b828033bebd99ff8a69c3c7bc32289
Nick Maludy <nmaludy@gmail.com>
2020-04-21 21:14:56 -0400

    Fixing RHEL 6 and 7 not having repoquery -y

--- scripts/st2bootstrap-el6.sh
+++ scripts/st2bootstrap-el6.sh
@@ -428,35 +428,42 @@ install_yum_utils() {
 get_full_pkg_versions() {
   if [ "$VERSION" != '' ];
   then
-    local ST2_VER=$(repoquery -y --nvr --show-duplicates st2 | grep -F st2-${VERSION} | sort --version-sort | tail -n 1)
+    local RHMAJVER=`cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9.]\).*/\1/'`
+    local REPOQUERY_FLAGS="--nvr --show-duplicates"
+    if [ "$RHMAJVER" -ge "8" ]; then
+      # RHEL 8 and newer, you need "-y" flag to avoid being prompted to confirm "yes"
+      local REPOQUERY_FLAGS="-y ${REPOQUERY_FLAGS}"
+    fi
+
+    local ST2_VER=$(repoquery ${REPOQUERY_FLAGS} st2 | grep -F st2-${VERSION} | sort --version-sort | tail -n 1)
     if [ -z "$ST2_VER" ]; then
       echo "Could not find requested version of st2!!!"
-      sudo repoquery -y --nvr --show-duplicates st2
+      sudo repoquery ${REPOQUERY_FLAGS} st2
       exit 3
     fi
     ST2_PKG=${ST2_VER}
 
-    local RHMAJVER=`cat /etc/redhat-release | sed 's/[^0-9.]*\([0-9.]\).*/\1/'`
-    local ST2MISTRAL_VER=$(repoquery -y --nvr --show-duplicates st2mistral | grep -F st2mistral-${VERSION} | sort --version-sort | tail -n 1)
-    if [ -z "$ST2MISTRAL_VER" && "$RHMAJVER" != '8' ]; then
+    local ST2MISTRAL_VER=$(repoquery ${REPOQUERY_FLAGS} st2mistral | grep -F st2mistral-${VERSION} | sort --version-sort | tail -n 1)
+    # RHEL 8 and newer does not install Mistral
+    if [ -z "$ST2MISTRAL_VER" -a "$RHMAJVER" -lt "8" ]; then
       echo "Could not find requested version of st2mistral!!!"
-      sudo repoquery -y --nvr --show-duplicates st2mistral
+      sudo repoquery ${REPOQUERY_FLAGS} st2mistral
       exit 3
     fi
     ST2MISTRAL_PKG=${ST2MISTRAL_VER}
 
-    local ST2WEB_VER=$(repoquery -y --nvr --show-duplicates st2web | grep -F st2web-${VERSION} | sort --version-sort | tail -n 1)
+    local ST2WEB_VER=$(repoquery ${REPOQUERY_FLAGS} st2web | grep -F st2web-${VERSION} | sort --version-sort | tail -n 1)
     if [ -z "$ST2WEB_VER" ]; then
       echo "Could not find requested version of st2web."
-      sudo repoquery -y --nvr --show-duplicates st2web
+      sudo repoquery ${REPOQUERY_FLAGS} st2web
       exit 3
     fi
     ST2WEB_PKG=${ST2WEB_VER}
 
-    local ST2CHATOPS_VER=$(repoquery -y --nvr --show-duplicates st2chatops | grep -F st2chatops-${VERSION} | sort --version-sort | tail -n 1)
+    local ST2CHATOPS_VER=$(repoquery ${REPOQUERY_FLAGS} st2chatops | grep -F st2chatops-${VERSION} | sort --version-sort | tail -n 1)
     if [ -z "$ST2CHATOPS_VER" ]; then
       echo "Could not find requested version of st2chatops."
-      sudo repoquery -y --nvr --show-duplicates st2chatops
+      sudo repoquery ${REPOQUERY_FLAGS} st2chatops
       exit 3
     fi
     ST2CHATOPS_PKG=${ST2CHATOPS_VER}
