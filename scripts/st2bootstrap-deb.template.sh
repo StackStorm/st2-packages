ef746492085e99bfac7c328013754ca0826d9dc7
JP Bourget <jp.bourget@gmail.com>
2020-06-29 11:32:36 -0400

    update el7 and xenial to usse mongo 4.0

--- scripts/st2bootstrap-deb.template.sh
+++ scripts/st2bootstrap-deb.template.sh
@@ -138,9 +138,9 @@ install_st2_dependencies() {
 }
 
 install_mongodb() {
-  # Add key and repo for the latest stable MongoDB (3.4)
-  # TODO: Install MongoDB 4.0 on Bionic
-  if [[ "$SUBTYPE" == 'bionic' ]]; then
+  # Add key and repo for the latest stable MongoDB (3.6 or 4.0)
+  # Install MongoDB 4.0 on xenial and bionic, else install Mongo 3.6
+  if [[ "${SUBTYPE}" == "xenial" || "${SUBTYPE}" == "bionic" ]]; then
     wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
     echo "deb http://repo.mongodb.org/apt/ubuntu ${SUBTYPE}/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
   else
@@ -155,7 +155,7 @@ install_mongodb() {
   # Configure MongoDB to listen on localhost only
   sudo sed -i -e "s#bindIp:.*#bindIp: 127.0.0.1#g" /etc/mongod.conf
 
-  if [[ "$SUBTYPE" == 'xenial' || "${SUBTYPE}" == "bionic" ]]; then
+  if [[ "${SUBTYPE}" == "xenial" || "${SUBTYPE}" == "bionic" ]]; then
     sudo systemctl enable mongod
     sudo systemctl start mongod
   else
