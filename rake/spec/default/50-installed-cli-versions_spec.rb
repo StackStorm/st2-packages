01ab261645fcdd25d85c2aa7ccf34a580622633e
armab <dev@arma.biz>
2020-12-08 14:03:00 +0000

    Test if st2 cli is running under the python 3.6

--- rake/spec/default/50-installed-cli-versions_spec.rb
+++ rake/spec/default/50-installed-cli-versions_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'st2 cli version checks' do
   describe command("st2 --version") do
     its(:exit_status) { is_expected.to eq 0 }
+    its(:stderr) { should match /on Python 3.6/ }
     # show version number in Rspec output
     after(:all) do
       puts puts "    " + described_class.stderr
