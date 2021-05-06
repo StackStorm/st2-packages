e106c658b86bf92cc86d97905526bc29ff62cdb8
W Chan <m4d.coder@gmail.com>
2021-05-06 00:06:35 +0000

    Add redis to docker compose

--- scripts/generate_st2_config.sh
+++ scripts/generate_st2_config.sh
@@ -8,6 +8,7 @@ set -e
 # --- Go!
 MONGOHOST="${MONGODBHOST:-mongodb}"
 RABBITMQHOST="${RABBITMQHOST:-rabbitmq}"
+REDISHOST="${REDISHOST:-redis}"
 
 CONF=/etc/st2/st2.conf
 AMQP="amqp://guest:guest@$RABBITMQHOST:5672/"
@@ -20,6 +21,7 @@ EHD
 # Don't join into one cmd with previous, otherwise it becomes
 # non-interactive waiting ^D.
 MONGO=$(echo "$MONGO" | sed -r 's/^\s+//')
+REDIS="redis://${REDISHOST}:6379"
 
 # Specify rabbitmq host
 sed -i "/\[messaging\]/,/\[.*\]\|url/ {n; s#url.*=.*#url = $AMQP#}" $CONF
@@ -27,5 +29,8 @@ sed -i "/\[auth\]/,/\[.*\]\|enable/ {n; s#enable.*=.*#enable = False#}" $CONF
 
 # Create database section, st2.conf ships without it
 (grep "\[database\]" $CONF &>/dev/null) || echo "$MONGO" >> /etc/st2/st2.conf
+
+# Specify redis host
+sed -i "/\[coordination\]/,/\[.*\]\|url/ {n; s#url.*=.*#url = $REDIS#}" $CONF
 
 echo  "Resulting $CONF >>>" "$(cat $CONF)"
