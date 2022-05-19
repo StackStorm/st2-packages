1ff7a52ad519b2a7e6ed7c697338ad4b740a2602
Amanda McGuinness <amanda.mcguinness@intive.com>
2022-05-19 20:33:13 +0100

    Apply suggestions from code review
    Co-authored-by: Jacob Floyd <cognifloyd@gmail.com>

--- scripts/st2bootstrap-el8.template.sh
+++ scripts/st2bootstrap-el8.template.sh
@@ -148,7 +148,7 @@ install_st2_dependencies() {
 install_rabbitmq() {
   # Install erlang from rabbitmq/erlang as need newer version
   # than available in epel.
-  # Use Erlang 24 has Erlang 25 has only preview support with RabbitMQ 3.10
+  # Use Erlang 24 as RabbitMQ 3.10 only has preview support for Erlang 25.
   curl -sL https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
   sudo yum -y install erlang-24*
   # Install rabbit from packagecloud
