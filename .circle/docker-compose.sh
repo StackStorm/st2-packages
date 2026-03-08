e64995035963bd67d8f29f9b184b2ea05e642f20
Carlos <nzlosh@yahoo.com>
2026-03-08 09:50:47 +0100

    Use 4 space indentation.

--- .circle/docker-compose.sh
+++ .circle/docker-compose.sh
@@ -14,36 +14,36 @@ set -e
 
 set -x
 case "$1" in
-  # Clean up cached Docker containers from the previous CircleCI build
-  # With https://circleci.com/docs/2.0/docker-layer-caching/ and 'reusable: true' we may see
-  # containers running from the previous cached build
-  clean)
-    echo Cleaning cached Docker containers which could be there from the previous build ...
-    docker compose -f docker-compose.circle.yml -f docker-compose.override.yml rm -v --stop --force || true
-  ;;
-  # Perform fake command invocation, technically provides images "pull" phase.
-  pull)
-    echo Pulling dependent Docker images for $2 ...
-    docker compose -f docker-compose.circle.yml -f docker-compose.override.yml pull --include-deps $2
-  ;;
-  build)
-    echo Starting Packages Build for $2 ...
-    docker compose -f docker-compose.circle.yml -f docker-compose.override.yml run \
-        -e ST2_CHECKOUT=${ST2_CHECKOUT} \
-        -e ST2_GITURL=${ST2_GITURL} \
-        -e ST2_GITREV=${ST2_GITREV} \
-        -e ST2_GITDIR=${ST2_GITDIR} \
-        -e ST2PKG_VERSION=${ST2PKG_VERSION} \
-        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
-        -e ST2_PACKAGES="${ST2_PACKAGES}" \
-        -e ST2_CIRCLE_URL="${CIRCLE_BUILD_URL}" \
-        $2 build
-  ;;
-  test)
-    [ "$TESTING" = 0 ] && { echo "Omitting Tests for $2 ..." ; exit 0; }
-    echo Starting Tests for $2 ...
-    docker compose -f docker-compose.circle.yml -f docker-compose.override.yml run \
-        -e ST2_PACKAGES="${ST2_PACKAGES}" \
-        "$2" test
-  ;;
+    clean)
+        # Clean up cached Docker containers from the previous CircleCI build
+        # With https://circleci.com/docs/2.0/docker-layer-caching/ and 'reusable: true' we may see
+        # containers running from the previous cached build
+        echo "Cleaning cached Docker containers which could be there from the previous build ..."
+        docker compose -f docker-compose.circle.yml -f docker-compose.override.yml rm -v --stop --force || true
+    ;;
+    pull)
+        # Perform fake command invocation, technically provides images "pull" phase.
+        echo "Pulling dependent Docker images for $2 ..."
+        docker compose -f docker-compose.circle.yml -f docker-compose.override.yml pull --include-deps "$2"
+    ;;
+    build)
+        echo "Starting Packages Build for $2 ..."
+        docker compose -f docker-compose.circle.yml -f docker-compose.override.yml run \
+            -e ST2_CHECKOUT=${ST2_CHECKOUT} \
+            -e ST2_GITURL=${ST2_GITURL} \
+            -e ST2_GITREV=${ST2_GITREV} \
+            -e ST2_GITDIR=${ST2_GITDIR} \
+            -e ST2PKG_VERSION=${ST2PKG_VERSION} \
+            -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
+            -e ST2_PACKAGES="${ST2_PACKAGES}" \
+            -e ST2_CIRCLE_URL="${CIRCLE_BUILD_URL}" \
+            "$2" build
+    ;;
+    test)
+        [ "$TESTING" = 0 ] && { echo "Omitting Tests for $2 ..." ; exit 0; }
+        echo "Starting Tests for $2 ..."
+        docker compose -f docker-compose.circle.yml -f docker-compose.override.yml run \
+            -e ST2_PACKAGES="${ST2_PACKAGES}" \
+            "$2" test
+    ;;
 esac
