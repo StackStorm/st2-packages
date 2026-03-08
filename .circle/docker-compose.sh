18cee729981aaf7ed9ab73884ffee99acd7a794c
Carlos <nzlosh@yahoo.com>
2026-03-08 09:45:22 +0100

    Remove unused docker-compose script

--- .circle/docker-compose.sh
+++ .circle/docker-compose.sh
@@ -9,55 +9,41 @@
 
 set -e
 # Source the build environment defintion (details in buildenv.sh)
+# shellcheck disable=SC1090
 . ~/.buildenv
-
-# Used for `RABBITMQHOST`, `MONGODBHOST`, and `REDISHOST` see docker-compose.override.yml
-HOST_IP=$(ifconfig docker0 | grep 'inet addr' | awk -F: '{print $2}' | awk '{print $1}')
 
 set -x
 case "$1" in
+  # Clean up cached Docker containers from the previous CircleCI build
+  # With https://circleci.com/docs/2.0/docker-layer-caching/ and 'reusable: true' we may see
+  # containers running from the previous cached build
+  clean)
+    echo Cleaning cached Docker containers which could be there from the previous build ...
+    docker compose -f docker-compose.circle2.yml -f docker-compose.override.yml rm -v --stop --force || true
+  ;;
   # Perform fake command invocation, technically provides images "pull" phase.
   pull)
     echo Pulling dependent Docker images for $2 ...
-    docker-compose -f docker-compose.circle.yml run \
-        -e ST2_GITURL=${ST2_GITURL} \
-        -e ST2_GITREV=${ST2_GITREV} \
-        -e ST2PKG_VERSION=${ST2PKG_VERSION} \
-        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
-        -e RABBITMQHOST=${HOST_IP} \
-        -e MONGODBHOST=${HOST_IP} \
-        -e REDISHOST=${HOST_IP} \
-        -e ST2_CIRCLE_URL=${CIRCLE_BUILD_URL} \
-        $2 /bin/true
+    docker compose -f docker-compose.circle2.yml -f docker-compose.override.yml pull --include-deps $2
   ;;
   build)
     echo Starting Packages Build for $2 ...
-    docker-compose -f docker-compose.circle.yml run \
+    docker compose -f docker-compose.circle2.yml -f docker-compose.override.yml run \
+        -e ST2_CHECKOUT=${ST2_CHECKOUT} \
         -e ST2_GITURL=${ST2_GITURL} \
         -e ST2_GITREV=${ST2_GITREV} \
+        -e ST2_GITDIR=${ST2_GITDIR} \
         -e ST2PKG_VERSION=${ST2PKG_VERSION} \
         -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
-        -e RABBITMQHOST=${HOST_IP} \
-        -e MONGODBHOST=${HOST_IP} \
-        -e REDISHOST=${HOST_IP} \
         -e ST2_PACKAGES="${ST2_PACKAGES}" \
-        -e ST2_CIRCLE_URL=${CIRCLE_BUILD_URL} \
+        -e ST2_CIRCLE_URL="${CIRCLE_BUILD_URL}" \
         $2 build
   ;;
   test)
     [ "$TESTING" = 0 ] && { echo "Omitting Tests for $2 ..." ; exit 0; }
     echo Starting Tests for $2 ...
-    docker-compose -f docker-compose.circle.yml run \
-        -e ST2_GITURL=${ST2_GITURL} \
-        -e ST2_GITREV=${ST2_GITREV} \
-        -e ST2PKG_VERSION=${ST2PKG_VERSION} \
-        -e ST2PKG_RELEASE=${ST2PKG_RELEASE} \
-        -e RABBITMQHOST=${HOST_IP} \
-        -e MONGODBHOST=${HOST_IP} \
-        -e REDISHOST=${HOST_IP} \
+    docker compose -f docker-compose.circle2.yml -f docker-compose.override.yml run \
         -e ST2_PACKAGES="${ST2_PACKAGES}" \
-        -e ST2_CIRCLE_URL=${CIRCLE_BUILD_URL} \
-        $2 test
+        "$2" test
   ;;
 esac
-
